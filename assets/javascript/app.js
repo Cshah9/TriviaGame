e7263bd532719837bd89ed8471baee884d8d0af0
Chirag K Shah <chiragkshah@Chirags-MBP.home>
2017-04-29 05:24:29 -0400

    handles multiple questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,27 +4,31 @@ var $jumbo;
 var $question;
 var intervalId;
 var time =30;
+var inProgress = false;
 // var thirtySecs = 30000;
 
 var question1 = {
 	question:"What is your favorite color?",
-	correctAnswer: "Blue",
+	correctAnswer: ["Sorry, trick question!"],
 	answers: [ "Blue", "Yellow", "Red", "Green"]
 };
 
 var question2 = {
-	question:"Really, what is your favorite color?",
-	correctAnswer: "Blue",
+	question:"Just kidding. Really, what is your favorite color?",
+	correctAnswer: [ "Blue", "Yellow", "Red", "Green"],
 	answers: [ "Blue", "Yellow", "Red", "Green"]
 };
 
 var question3 = {
 	question:"What color is something that has no color?",
-	correctAnswer: "Clear",
-	answers:[ "White", "Black", "Fuscia", "Clear"]
+	correctAnswer: ["Clear", "None"],
+	answers:[ "White", "Black", "Fuscia", "Clear", "None", "None of the above"]
 };
 var currentQuestion = question1;
+var currentQuestionIndex=0;
 var qs = [question1, question2, question3];
+var correct = 0;
+var wrong = 0;
 
 
 $(document).ready(function(){
@@ -41,12 +45,101 @@ $(document).ready(function(){
 function startGame() {
 	console.log("start");
 	//remove instruction jumbotron;
-	$jumbo = $("#instructions").detach();
-	$("#main").append(formatQuestion());
-	$(".option").click(checkAnswer);
-	$("#display").html(convertTime(time));
-	intervalId = setInterval(countdown, 1000);
+	if(!inProgress){
+		inProgress = true;
+		$jumbo = $("#instructions").detach();
+		$("#main").append(formatQuestion(qs[currentQuestionIndex]));
+		$(".option").click(checkAnswer);
+		$("#display").html(convertTime(time));
+		intervalId = setInterval(countdown, 1000);
+	}
+}
 
+function nextQuestion(isCorrect){
+	console.log("nextQuestion : " + isCorrect + " : time - " + time);
+
+	var aStr = "";
+	
+	if (time==30){
+		//user ran out of time
+		console.log("warning panel");
+		aStr += '<div class="panel panel-warning message">';
+		aStr += '  <div class="panel-heading">';
+		aStr += '    <h3 class="panel-title">Out of time</h3>';
+		aStr += '  </div>';
+		aStr += '  <div class="panel-body">';
+		aStr += "    That's a bit of shite, i'nit!";
+		aStr += '  	 <br>';
+		aStr += '  	 <br>';
+		aStr += '  	 The correct answer is: ' + qs[currentQuestionIndex].correctAnswer[0];
+		aStr += '  	 <br>';
+		aStr += '  	 <br>';
+		aStr += '    Preparing next question...';
+		aStr += '  </div>';
+		aStr += '</div>';
+		wrong++;
+	}
+	else if (isCorrect) {
+		//if the answer is correct
+		console.log("success panel");
+		aStr += '<div class="panel panel-success message">';
+	  	aStr += '  <div class="panel-heading">';
+		aStr += '    <h3 class="panel-title">Correct</h3>';
+		aStr += '  </div>';
+		aStr += '  <div class="panel-body">';
+		aStr += '    Well done! Cheers, mate!';
+		aStr += '  	 <br>';
+		aStr += '  	 <br>';
+		aStr += '    Preparing next question...';
+		aStr += '  </div>';
+		aStr += '</div>';
+		correct ++;
+	}
+	else {
+		// else answer is wrong
+		console.log("danger panel");
+		aStr += '<div class="panel panel-danger message">';
+		aStr += '  <div class="panel-heading">';
+		aStr += '    <h3 class="panel-title">Wrong</h3>';
+		aStr += '  </div>';
+		aStr += '  <div class="panel-body">';
+		aStr += '    Bloody hell! Buggered that one!';
+		aStr += '  	 <br>';
+		aStr += '  	 <br>';
+		aStr += '  	 The correct answer is: ' + qs[currentQuestionIndex].correctAnswer[0];
+		aStr += '  	 <br>';
+		aStr += '  	 <br>';
+		aStr += '    Preparing next question...';
+		aStr += '  </div>';
+		aStr += '</div>';
+		wrong++;
+	}
+	clearInterval(intervalId);
+	//reset clock
+	time = 30;
+	$("#display").html(convertTime(time));
+	//add instruction jumbotron;
+	$(".question").detach()
+	$("#main").append(aStr);
+	//Check if out of questions:
+	setTimeout(startNextRound, 3000);
+}
+
+function startNextRound(){
+	console.log("startNextRound");
+
+	$(".message").detach();
+	if(currentQuestionIndex == qs.length -1) {
+
+		resetGame();
+		return;
+	}
+	else {
+		currentQuestionIndex++;
+	}
+	$("#main").append(formatQuestion(qs[currentQuestionIndex]));
+	$(".option").click(checkAnswer);
+	intervalId = setInterval(countdown, 1000);
 }
 
 function countdown() {
@@ -69,13 +162,14 @@ function countdown() {
 
 
 }
+
 function timesUp(){
 	// clear interval
 	clearInterval(intervalId);
 	//reset clock
 	time = 30;
 	//setup next question....
-	resetGame();
+	nextQuestion(false);
 
 }
 
@@ -83,28 +177,37 @@ function resetGame() {
 	console.log("reset");
 	// clear interval
 	clearInterval(intervalId);
+	inProgress = false;
 	//reset clock
 	time = 30;
 	$("#display").html(convertTime(time));
 	//add instruction jumbotron;
+	$(".message").detach()
 	$(".question").detach()
 	$("#main").append($jumbo);
+	$("#correct").text(correct);
+	$("#wrong").text(wrong);
+	currentQuestionIndex = 0;
+	correct = 0;
+	wrong = 0;
 }
 
 
-function formatQuestion(){
+function formatQuestion(q){
 	//string for question html:
 	var qStr="";
 
 	qStr+='  	<div class="panel panel-primary question">';
 	qStr+='  			  <div class="panel-heading">';
-	qStr+='  			    <h3 class="panel-title">Question : What is your favorite color?</h3>';
+	qStr+='  			    <h3 class="panel-title">Question : ' + q.question + '</h3>';
 	qStr+='  			  </div>';
-	qStr+='  			  <div class="panel-body">';	    
-	qStr+='  				<h2 class="option">Blue</h2>';
-	qStr+='  				<h2 class="option">Yellow</h2>';
-	qStr+='  				<h2 class="option">Red</h2>';
-	qStr+='  				<h2 class="option">Green</h2>';
+	qStr+='  			  <div class="panel-body">';	
+
+	for (var i = q.answers.length - 1; i >= 0; i--) {
+		
+		qStr+='  				<h2 class="option">' + q.answers[i] + '</h2>';
+	}
+
 	qStr+='  			</div>';
 	qStr+='  		</div>';
 
@@ -116,7 +219,17 @@ function checkAnswer(){
 	console.log("check answer");
 	console.log($(this).text().toLowerCase() );
 	console.log(currentQuestion);
-	console.log($(this).text().toLowerCase() == currentQuestion.correctAnswer.toLowerCase());
+
+
+	for (var i = qs[currentQuestionIndex].correctAnswer.length - 1; i >= 0; i--) {
+		if (qs[currentQuestionIndex].correctAnswer[i].toLowerCase() === $(this).text().toLowerCase()) {
+			console.log(true);
+			nextQuestion(true);
+			return;
+		}
+	}
+	console.log(false);
+	nextQuestion(false);
 
 }
 
